6933d83f04e52aa46ae237ed3666211c86a478c0
Fachrul Rivaldy <fachrul.rivaldy13@gmail.com>
2021-11-03 15:16:30 +0700

    Chatty 4

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'theme.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -6,7 +7,40 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Image.asset('assets/images/profpic.png'),
+      backgroundColor: blueColor,
+      body: SafeArea(
+          child: Center(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 40,
+            ),
+            Image.asset(
+              'assets/images/profpic.png',
+              width: 100,
+              height: 100,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              "Sabrina Carpenter",
+              style: TextStyle(
+                  color: whiteColor, fontSize: 20, fontWeight: FontWeight.w400),
+            ),
+            SizedBox(
+              height: 2,
+            ),
+            Text(
+              "Travel Freelancer",
+              style: TextStyle(
+                  color: lightBlueColor,
+                  fontSize: 16,
+                  fontWeight: FontWeight.w300),
+            ),
+          ],
+        ),
+      )),
     );
   }
 }
