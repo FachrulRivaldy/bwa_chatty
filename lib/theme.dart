2c4d552e6ca3832690c443a1d04b677075ecc991
Fachrul Rivaldy <fachrul.rivaldy13@gmail.com>
2021-11-03 14:50:09 +0700

    Chatty 3

---
+++ lib/theme.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+Color blueColor = Color(0xFF1F8DF5);
+Color whiteColor = Color(0xFFFFFFFF);
+Color lightBlueColor = Color(0xFF9ED0FF);
+Color blackColor = Color(0xFF2C3A59);
+Color grayColor = Color(0xFF808BA2);
+Color greenColor = Color(0xFF29CB9E);
+Color sendGrayColor = Color(0xFFEAEFF3);
+Color sendBlackColor = Color(0xFF505C6B);
+
+TextStyle titleTextStyle =
+    TextStyle(color: blackColor, fontWeight: FontWeight.w500, fontSize: 16);
+
+TextStyle subtitleTextStyle =
+    TextStyle(color: grayColor, fontWeight: FontWeight.w300, fontSize: 14);
