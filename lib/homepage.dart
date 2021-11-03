9e97a67c33982e42ba891f1ceb14d5e7c72cc866
Fachrul Rivaldy <fachrul.rivaldy13@gmail.com>
2021-11-03 14:08:37 +0700

    Chatty 2

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -6,7 +6,7 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text("Home Page"),
+      body: Image.asset('assets/images/profpic.png'),
     );
   }
 }
